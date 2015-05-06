c55037e45b0853b752ca1db85b95674c053186aa
Ian Unruh <ianunruh@gmail.com>
2015-05-05 20:56:35 -0700

    Refactor error handling

--- vault/tests/test_integration.py
+++ vault/tests/test_integration.py
@@ -1,12 +1,17 @@
 import os
 
-from vault import Client
+from nose.tools import *
 
-def test_generic_secret_backend():
-    client = Client(
+from vault import Client, exceptions
+
+def create_client():
+    return Client(
         token=os.environ['VAULT_TOKEN'],
         url='http://localhost:8200',
     )
+
+def test_generic_secret_backend():
+    client = create_client()
 
     client.write('secret/foo', zap='zip')
     result = client.read('secret/foo')
@@ -14,3 +19,13 @@ def test_generic_secret_backend():
     assert result['data']['zap'] == 'zip'
 
     client.delete('secret/foo')
+
+@raises(exceptions.InvalidPath)
+def test_invalid_path():
+    client = create_client()
+    client.read('secret/I/do/not/exist')
+
+@raises(exceptions.InternalServerError)
+def test_internal_server_error():
+    client = create_client()
+    client.read('handler/does/not/exist')
--- vault/v1/__init__.py
+++ vault/v1/__init__.py
@@ -1,39 +1,72 @@
 import requests
 
+from vault import exceptions
+
+def raise_for_status(func):
+    def decorator(*args, **kwargs):
+        response = func(*args, **kwargs)
+
+        if response.status_code >= 400 and response.status_code < 600:
+            errors = response.json().get('errors')
+
+            if response.status_code == 400:
+                raise exceptions.InvalidRequest(errors=errors)
+            elif response.status_code == 401:
+                raise exceptions.Unauthorized(errors=errors)
+            elif response.status_code == 404:
+                raise exceptions.InvalidPath(errors=errors)
+            elif response.status_code == 429:
+                raise exceptions.RateLimitExceeded(errors=errors)
+            elif response.status_code == 500:
+                raise exceptions.InternalServerError(errors=errors)
+            elif response.status_code == 503:
+                raise exceptions.VaultDown(errors=errors)
+            else:
+                raise exceptions.UnknownError()
+
+        return response
+
+    return decorator
+
 class Client(object):
     def __init__(self, url, token):
         self._url = url
-        self._cookies = {'token': token}
+        self._token = token
 
     def read(self, path):
+        """
+        GET /<path>
+        """
         return self._get('/v1/{}'.format(path)).json()
 
     def write(self, path, **kwargs):
+        """
+        PUT /<path>
+        """
         self._put('/v1/{}'.format(path), kwargs)
 
     def delete(self, path):
+        """
+        DELETE /<path>
+        """
         self._delete('/v1/{}'.format(path))
 
+    @raise_for_status
     def _get(self, url, **kwargs):
-        url = self._url + url
+        return requests.get(self._url + url, cookies=self._cookies, **kwargs)
 
-        response = requests.get(url, cookies=self._cookies, **kwargs)
-        response.raise_for_status()
+    @raise_for_status
+    def _post(self, url, data, **kwargs):
+        return requests.post(self._url + url, json=data, cookies=self._cookies, **kwargs)
 
-        return response
-
+    @raise_for_status
     def _put(self, url, data, **kwargs):
-        url = self._url + url
+        return requests.put(self._url + url, json=data, cookies=self._cookies, **kwargs)
 
-        response = requests.put(url, json=data, cookies=self._cookies, **kwargs)
-        response.raise_for_status()
-
-        return response
-
+    @raise_for_status
     def _delete(self, url, **kwargs):
-        url = self._url + url
+        return requests.delete(self._url + url, cookies=self._cookies, **kwargs)
 
-        response = requests.delete(url, cookies=self._cookies, **kwargs)
-        response.raise_for_status()
-
-        return response
+    @property
+    def _cookies(self):
+        return {'token': self._token}
