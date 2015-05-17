ce2d4d6971988a5db6fdd0729ff9a805413dd6ce
Gary Wright <zerosignal0@msn.com>
2015-05-17 00:58:59 -0700

    Updated version of downloaded bin from 0.1.1 to 0.1.2 (current latest).

--- ci/start-vault-dev.sh
+++ ci/start-vault-dev.sh
@@ -3,8 +3,8 @@ set -eux
 
 mkdir bin
 
-curl -OL https://dl.bintray.com/mitchellh/vault/vault_0.1.1_linux_amd64.zip
-unzip vault_0.1.1_linux_amd64.zip -d bin
+curl -OL https://dl.bintray.com/mitchellh/vault/vault_0.1.2_linux_amd64.zip
+unzip vault_0.1.2_linux_amd64.zip -d bin
 
 bin/vault server -dev &
 
