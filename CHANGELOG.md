65bbcba086abed57d7905294fcf25d6cb5085caa
Ian Unruh <ianunruh@gmail.com>
2015-05-20 19:51:05 -0700

    Prepare for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 0.1.1 (unreleased)
+## 0.1.1 (May 20th, 2015)
 
 IMPROVEMENTS:
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hvac',
-    version='0.1.0',
+    version='0.1.1',
     description='HashiCorp Vault API client',
     author='Ian Unruh',
     author_email='ianunruh@gmail.com',
