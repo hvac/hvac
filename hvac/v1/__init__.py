5e1503698ee24fef0657bf9984d10c56cac38842
Ian Unruh <ianunruh@gmail.com>
2015-09-29 11:55:17 -0700

    Add support for PGP encryption for unseal keys
    Added in Vault 0.3.0

--- hvac/v1/__init__.py
+++ hvac/v1/__init__.py
@@ -44,7 +44,7 @@ class Client(object):
         """
         return self._get('/v1/sys/init').json()['initialized']
 
-    def initialize(self, secret_shares=5, secret_threshold=3):
+    def initialize(self, secret_shares=5, secret_threshold=3, pgp_keys=None):
         """
         PUT /sys/init
         """
@@ -52,6 +52,12 @@ class Client(object):
             'secret_shares': secret_shares,
             'secret_threshold': secret_threshold,
         }
+
+        if pgp_keys:
+            if len(pgp_keys) != secret_shares:
+                raise ValueError('Length of pgp_keys must equal secret shares')
+
+            params['pgp_keys'] = pgp_keys
 
         return self._put('/v1/sys/init', json=params).json()
 
