0fb430f0c8ee1782dc3869359ff7e71a432c88f8
Marcos Antonio Lopes <marcos.abil@gmail.com>
2016-09-13 02:11:07 +1000

    Remove json response double parsing (#76)

--- hvac/v1/__init__.py
+++ hvac/v1/__init__.py
@@ -515,7 +515,7 @@ class Client(object):
         if role:
             params['role'] = role
 
-        return self.auth('/v1/auth/aws-ec2/login', json=params, use_token=use_token).json()
+        return self.auth('/v1/auth/aws-ec2/login', json=params, use_token=use_token)
 
     def create_userpass(self, username, password, policies, mount_point='userpass', **kwargs):
         """
