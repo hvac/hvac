e976e70c8555e3cecbeaf0599ac087eaf4d681a8
Ram Amar <amar.rami@gmail.com>
2017-03-09 00:36:28 +0200

    create_role_secret_id(..) add wrap_ttl

--- hvac/v1/__init__.py
+++ hvac/v1/__init__.py
@@ -856,7 +856,7 @@ class Client(object):
         """
         return self._get('/v1/auth/approle/role/{0}'.format(role_name)).json()
 
-    def create_role_secret_id(self, role_name, meta=None):
+    def create_role_secret_id(self, role_name, meta=None, wrap_ttl=None):
         """
         POST /auth/approle/role/<role name>/secret-id
         """
@@ -866,7 +866,7 @@ class Client(object):
         if meta is not None:
             params['metadata'] = json.dumps(meta)
 
-        return self._post(url, json=params).json()
+        return self._post(url, json=params, wrap_ttl=wrap_ttl).json()
 
     def get_role_secret_id(self, role_name, secret_id):
         """
