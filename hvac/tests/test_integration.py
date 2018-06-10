4923f3db328e1e52d27c5e7217561c80943fb214
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-06-10 14:16:01 -0500

    Update test assertions to cover pre *and* post v0.9.6 Vault

--- hvac/tests/test_integration.py
+++ hvac/tests/test_integration.py
@@ -960,15 +960,15 @@ class IntegrationTest(TestCase):
         assert('grault' in roles['data']['keys'])
 
         foo_role = self.client.get_ec2_role('foo')
-        assert (foo_role['data']['bound_ami_id'] == ['ami-notarealami'])
+        assert ('ami-notarealami' in foo_role['data']['bound_ami_id'])
         assert ('ec2rolepolicy' in foo_role['data']['policies'])
 
         bar_role = self.client.get_ec2_role('bar')
-        assert (bar_role['data']['bound_account_id'] == ['123456789012'])
+        assert ('123456789012' in bar_role['data']['bound_account_id'])
         assert ('ec2rolepolicy' in bar_role['data']['policies'])
 
         baz_role = self.client.get_ec2_role('baz')
-        assert (baz_role['data']['bound_iam_role_arn'] == ['arn:aws:iam::123456789012:role/mockec2role'])
+        assert ('arn:aws:iam::123456789012:role/mockec2role' in baz_role['data']['bound_iam_role_arn'])
         assert ('ec2rolepolicy' in baz_role['data']['policies'])
 
         qux_role = self.client.get_ec2_role('qux')
