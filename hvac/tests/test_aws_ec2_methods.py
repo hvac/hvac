9c3c90b3eeb00fdef29c635635304d1bfbf12afb
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-06-11 13:59:15 -0500

    Rename and expand aws ec2 unit tests

--- hvac/tests/test_aws_ec2_methods.py
+++ hvac/tests/test_aws_ec2_methods.py
@@ -1,15 +1,20 @@
 from unittest import TestCase
 
 import requests_mock
-from nose.tools import *
+from parameterized import parameterized
 
 from hvac import Client
 
 
-class UnitTest(TestCase):
+class TestAwsEc2Methods(TestCase):
+    """Unit tests providing coverage for AWS (EC2) auth backend-related methods/routes."""
 
+    @parameterized.expand([
+        ("default mount point", None),
+        ("custom mount point", 'aws-ec2'),
+    ])
     @requests_mock.Mocker()
-    def test_auth_ec2(self, requests_mocker):
+    def test_auth_ec2(self, test_label, mount_point, requests_mocker):
         mock_response = {
             'auth': {
                 'accessor': 'accessor-1234-5678-9012-345678901234',
@@ -38,10 +43,25 @@ class UnitTest(TestCase):
             'warnings': [],
             'wrap_info': None
         }
-        test_mount_point = 'aws-ec2'
-        requests_mocker.register_uri('POST', 'http://localhost:8200/v1/auth/{0}/login'.format(test_mount_point), json=mock_response)
+        mock_url = 'http://localhost:8200/v1/auth/{0}/login'.format(
+            'aws-ec2' if mount_point is None else mount_point
+        )
+        requests_mocker.register_uri(
+            method='POST',
+            url=mock_url,
+            json=mock_response
+        )
         client = Client()
-        actual_response= client.auth_ec2('mock_pcks7')
-        
+
+        if mount_point is None:
+            actual_response = client.auth_ec2(
+                pkcs7='mock_pcks7'
+            )
+        else:
+            actual_response = client.auth_ec2(
+                pkcs7='mock_pcks7',
+                mount_point=mount_point
+            )
+
         # ensure we received our mock response data back successfully
         self.assertEqual(mock_response, actual_response)
