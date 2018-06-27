c2059b3e3a85c244fb3e886de22b816909a11911
Sean Malloy <spinelli85@gmail.com>
2018-06-26 23:32:51 -0500

    Initial GCP auth backend implementation

--- README.md
+++ README.md
@@ -75,6 +75,19 @@ client.auth_aws_iam(credentials.access_key, credentials.secret_key, credentials.
 # GitHub
 client.auth_github('MY_GITHUB_TOKEN')
 
+# GCP (from GCE instance)
+import requests
+
+VAULT_ADDR="https://vault.example.com:8200"
+ROLE="example"
+AUDIENCE_URL =  VAULT_ADDR + "/vault/" + ROLE
+METADATA_HEADERS = {'Metadata-Flavor': 'Google'}
+FORMAT = 'full'
+
+url = 'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience={}&format={}'.format(AUDIENCE_URL, FORMAT)
+r = requests.get(url, headers=METADATA_HEADERS)
+client.auth_gcp(ROLE, r.text)
+
 # LDAP, Username & Password
 client.auth_ldap('MY_USERNAME', 'MY_PASSWORD')
 client.auth_userpass('MY_USERNAME', 'MY_PASSWORD')
--- hvac/tests/test_integration.py
+++ hvac/tests/test_integration.py
@@ -1244,6 +1244,34 @@ class IntegrationTest(TestCase):
         self.client.token = self.root_token()
         self.client.disable_auth_backend(mount_point=test_mount_point)
 
+    def test_auth_gcp_alternate_mount_point_with_no_client_token_exception(self):
+        test_mount_point = 'gcp-custom-path'
+        # Turn on the gcp backend with a custom mount_point path specified.
+        if '{0}/'.format(test_mount_point) in self.client.list_auth_backends():
+            self.client.disable_auth_backend(test_mount_point)
+        self.client.enable_auth_backend('gcp', mount_point=test_mount_point)
+
+        # Drop the client's token to replicate a typical end user's use of any auth method.
+        # I.e., its reasonable to expect the method is being called to _retrieve_ a token in the first place.
+        self.client.token = None
+
+        # Load a mock JWT stand in for a real document from GCP.
+        with open('test/example.jwt') as fp:
+            jwt = fp.read()
+
+        # When attempting to auth (POST) to an auth backend mounted at a different path than the default, we expect a
+        # generic 'missing client token' response from Vault.
+        with self.assertRaises(exceptions.InvalidRequest) as assertRaisesContext:
+            self.client.auth_gcp('example-role', jwt)
+
+        expected_exception_message = 'missing client token'
+        actual_exception_message = str(assertRaisesContext.exception)
+        self.assertEqual(expected_exception_message, actual_exception_message)
+
+        # Reset test state.
+        self.client.token = self.root_token()
+        self.client.disable_auth_backend(mount_point=test_mount_point)
+
     def test_tune_auth_backend(self):
         test_backend_type = 'approle'
         test_mount_point = 'tune-approle'
