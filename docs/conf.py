81c6fe15b1fc2aca7597023294eb2d3e909960df
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-07-16 08:25:31 -0500

    Include class constructors with autodoc modules

--- docs/conf.py
+++ docs/conf.py
@@ -67,4 +67,14 @@ epub_copyright = copyright
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
 
-# -- Extension configuration -------------------------------------------------
+# -- Autodoc configuration -------------------------------------------------
+
+
+def skip(app, what, name, obj, skip, options):
+    if name == "__init__":
+        return False
+    return skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
