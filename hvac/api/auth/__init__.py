32972a6346398c98148a4cf59257f282ef0b6e5c
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-08-06 08:45:52 -0500

    Add Mfa class

--- hvac/api/auth/__init__.py
+++ hvac/api/auth/__init__.py
@@ -1,7 +1,9 @@
 """Collection of classes for various Vault auth methods."""
 
 from hvac.api.auth.github import Github
+from hvac.api.auth.mfa import Mfa
 
 __all__ = (
     'Github',
+    'Mfa',
 )
