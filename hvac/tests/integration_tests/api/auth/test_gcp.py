a4d0f5cf9d009748c2873a64677f9b123de1c2a7
Sean Malloy <spinelli85@gmail.com>
2018-08-15 22:24:11 -0500

    fix white space

--- hvac/tests/integration_tests/api/auth/test_gcp.py
+++ hvac/tests/integration_tests/api/auth/test_gcp.py
@@ -18,64 +18,62 @@ class TestGcp(utils.HvacIntegrationTestCase, TestCase):
     def tearDown(self):
         super(TestGcp, self).tearDown()
 
-    
     @parameterized.expand([
         ('some_test',),
     ])
     def test_configure(self, test_label):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     def test_read_config(self, test_label):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     def test_delete_config(self, test_label):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     def test_create_role(self, test_label):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     def test_edit_service_accounts_on_iam_role(self, test_label):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     def test_edit_labels_on_gce_role(self, test_label):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     def test_read_role(self, test_label):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     def test_list_roles(self, test_label):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     def test_delete_role(self, test_label):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     def test_login(self, test_label):
         raise NotImplementedError
-    
