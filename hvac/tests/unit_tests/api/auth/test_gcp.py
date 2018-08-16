00e5ba32dd400bb796813f90f1df560598b219d9
Sean Malloy <spinelli85@gmail.com>
2018-08-15 22:26:43 -0500

    fix white space

--- hvac/tests/unit_tests/api/auth/test_gcp.py
+++ hvac/tests/unit_tests/api/auth/test_gcp.py
@@ -20,74 +20,72 @@ class TestGcp(TestCase):
     def tearDown(self):
         super(TestGcp, self).tearDown()
 
-    
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_configure(self, test_label, requests_mocker):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_read_config(self, test_label, requests_mocker):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_delete_config(self, test_label, requests_mocker):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_create_role(self, test_label, requests_mocker):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_edit_service_accounts_on_iam_role(self, test_label, requests_mocker):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_edit_labels_on_gce_role(self, test_label, requests_mocker):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_read_role(self, test_label, requests_mocker):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_list_roles(self, test_label, requests_mocker):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_delete_role(self, test_label, requests_mocker):
         raise NotImplementedError
-    
+
     @parameterized.expand([
         ('some_test',),
     ])
     @requests_mock.Mocker()
     def test_login(self, test_label, requests_mocker):
         raise NotImplementedError
-    
