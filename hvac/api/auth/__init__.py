759c30a16349562c4a3ad87572394c09b1501761
Sean Malloy <spinelli85@gmail.com>
2018-08-15 22:08:31 -0500

    Fix it

--- hvac/api/auth/__init__.py
+++ hvac/api/auth/__init__.py
@@ -1,10 +1,12 @@
 """Collection of classes for various Vault auth methods."""
 
+from hvac.api.auth.gcp import Gcp
 from hvac.api.auth.github import Github
 from hvac.api.auth.ldap import Ldap
 from hvac.api.auth.mfa import Mfa
 
 __all__ = (
+    'Gcp',
     'Github',
     'Ldap',
     'Mfa',
