316c095b8fc5b1d01bf480ba4ceab2be2868794c
rastut <litus81@gmail.com>
2018-08-22 10:50:25 +0200

    Send token as a param, to avoid token on the path because it's logged by the audit log

--- hvac/v1/__init__.py
+++ hvac/v1/__init__.py
@@ -931,7 +931,7 @@ class Client(object):
         self._adapter.post('/v1/auth/token/revoke-prefix/{0}'.format(prefix))
 
     def renew_token(self, token=None, increment=None, wrap_ttl=None):
-        """POST /auth/token/renew/<token>
+        """POST /auth/token/renew
 
         POST /auth/token/renew-self
 
@@ -946,11 +946,11 @@ class Client(object):
         """
         params = {
             'increment': increment,
+            'token': token,
         }
 
         if token:
-            path = '/v1/auth/token/renew/{0}'.format(token)
-            return self._adapter.post(path, json=params, wrap_ttl=wrap_ttl).json()
+            return self._adapter.post('/v1/auth/token/renew', json=params, wrap_ttl=wrap_ttl).json()
         else:
             return self._adapter.post('/v1/auth/token/renew-self', json=params, wrap_ttl=wrap_ttl).json()
 
