5884ec7aa8dfd09dc2e6070269d0083ac2b57cb9
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-09-04 09:45:17 -0500

    Update tests for kv_v2

--- hvac/tests/unit_tests/api/secrets_engines/test_kv.py
+++ hvac/tests/unit_tests/api/secrets_engines/test_kv.py
@@ -5,6 +5,7 @@ from parameterized import parameterized
 
 from hvac.api.secrets_engines.kv import Kv
 from hvac.api.secrets_engines.kv_v1 import KvV1
+from hvac.api.secrets_engines.kv_v2 import KvV2
 from hvac.tests import utils
 
 
@@ -21,17 +22,21 @@ class TestKv(utils.HvacIntegrationTestCase, TestCase):
     def test_v2_property(self):
         mock_adapter = MagicMock()
         kv = Kv(adapter=mock_adapter)
-        with self.assertRaises(NotImplementedError):
-            assert kv.v2
+        self.assertIsInstance(
+            obj=kv.v2,
+            cls=KvV2,
+        )
 
     @parameterized.expand([
         ('v1', '1'),
-        ('v2', '2', ValueError),
+        ('v2', '2'),
+        ('v3', '3', ValueError),
         ('invalid version', '12345', ValueError),
     ])
     def test_default_kv_version_setter(self, test_label, version, raises=False):
         version_class_map = {
             '1': KvV1,
+            '2': KvV2,
         }
         mock_adapter = MagicMock()
         kv = Kv(adapter=mock_adapter)
@@ -52,6 +57,11 @@ class TestKv(utils.HvacIntegrationTestCase, TestCase):
         self.assertEqual(
             first=kv.read_secret,
             second=kv.v1.read_secret,
+        )
+        kv = Kv(adapter=mock_adapter, default_kv_version='2')
+        self.assertEqual(
+            first=kv.read_secret_version,
+            second=kv.v2.read_secret_version,
         )
 
         kv._default_kv_version = 0
