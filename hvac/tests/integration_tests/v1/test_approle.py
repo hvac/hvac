420ce3ed87c770a5ee276c0c1ea8f192177f00ed
jeffwecan <jeff.hogan1@gmail.com>
2018-09-29 02:09:57 +0000

    Move approle test cases to their own class

--- hvac/tests/integration_tests/v1/test_approle.py
+++ hvac/tests/integration_tests/v1/test_approle.py
@@ -8,7 +8,7 @@ from hvac.tests import utils
 
 
 class TestApprole(utils.HvacIntegrationTestCase, TestCase):
-    TEST_MOUNT_POINT = 'test-approle'
+    TEST_MOUNT_POINT = 'approle'
 
     def setUp(self):
         super(TestApprole, self).setUp()
@@ -18,6 +18,7 @@ class TestApprole(utils.HvacIntegrationTestCase, TestCase):
         )
 
     def tearDown(self):
+        self.client.token = self.manager.root_token
         self.client.disable_auth_backend(mount_point=self.TEST_MOUNT_POINT)
         super(TestApprole, self).tearDown()
 
@@ -64,3 +65,69 @@ class TestApprole(utils.HvacIntegrationTestCase, TestCase):
                 first=num_secrets_to_create,
                 second=len(list_role_secrets_response['data']['keys'])
             )
+
+    def test_create_role(self):
+        self.client.create_role('testrole')
+
+        result = self.client.read('auth/approle/role/testrole')
+        lib_result = self.client.get_role('testrole')
+        del result['request_id']
+        del lib_result['request_id']
+
+        assert result == lib_result
+
+    def test_delete_role(self):
+        test_role_name = 'test-role'
+
+        self.client.create_role(test_role_name)
+        # We add a second dummy test role so we can still hit the /role?list=true route after deleting the first role
+        self.client.create_role('test-role-2')
+
+        # Ensure our created role shows up when calling list_roles as expected
+        result = self.client.list_roles()
+        actual_list_role_keys = result['data']['keys']
+        self.assertIn(
+            member=test_role_name,
+            container=actual_list_role_keys,
+        )
+
+        # Now delete the role and verify its absence when calling list_roles
+        self.client.delete_role(test_role_name)
+        result = self.client.list_roles()
+        actual_list_role_keys = result['data']['keys']
+        self.assertNotIn(
+            member=test_role_name,
+            container=actual_list_role_keys,
+        )
+
+    def test_create_delete_role_secret_id(self):
+        self.client.create_role('testrole')
+        create_result = self.client.create_role_secret_id('testrole', {'foo': 'bar'})
+        secret_id = create_result['data']['secret_id']
+        result = self.client.get_role_secret_id('testrole', secret_id)
+        assert result['data']['metadata']['foo'] == 'bar'
+        self.client.delete_role_secret_id('testrole', secret_id)
+        try:
+            self.client.get_role_secret_id('testrole', secret_id)
+            assert False
+        except (exceptions.InvalidPath, ValueError):
+            assert True
+
+    def test_auth_approle(self):
+        self.client.create_role('testrole')
+        create_result = self.client.create_role_secret_id('testrole', {'foo': 'bar'})
+        secret_id = create_result['data']['secret_id']
+        role_id = self.client.get_role_id('testrole')
+        result = self.client.auth_approle(role_id, secret_id)
+        assert result['auth']['metadata']['foo'] == 'bar'
+        assert self.client.token == result['auth']['client_token']
+        assert self.client.is_authenticated()
+
+    def test_auth_approle_dont_use_token(self):
+        self.client.create_role('testrole')
+        create_result = self.client.create_role_secret_id('testrole', {'foo': 'bar'})
+        secret_id = create_result['data']['secret_id']
+        role_id = self.client.get_role_id('testrole')
+        result = self.client.auth_approle(role_id, secret_id, use_token=False)
+        assert result['auth']['metadata']['foo'] == 'bar'
+        assert self.client.token != result['auth']['client_token']
