182b394132f713aac2b9c071791400a15d61d17c
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-10-01 21:17:07 -0500

    Add azure wrapper

--- hvac/api/__init__.py
+++ hvac/api/__init__.py
@@ -1,10 +1,12 @@
 """Collection of Vault API endpoint classes."""
 from hvac.api import auth
+from hvac.api.azure import Azure
 from hvac.api import secrets_engines
 from hvac.api.vault_api_base import VaultApiBase
 
 __all__ = (
-    'VaultApiBase',
     'auth',
+    'Azure',
     'secrets_engines',
+    'VaultApiBase',
 )
