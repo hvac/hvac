b91ebdeaab9b417e34309b02689ab6006b857cde
jeffwecan <jeff.hogan1@gmail.com>
2018-10-29 12:12:20 -0500

    new gcp constants source file

---
+++ hvac/constants/gcp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Constants related to GCP auth methods and/or secrets engines."""
+
+DEFAULT_MOUNT_POINT = 'gcp'
+ALLOWED_ROLE_TYPES = ['iam', 'gce']
+GCP_CERTS_ENDPOINT = 'https://www.googleapis.com/oauth2/v3/certs'
