415f701144b8e68ef40bea6431d63b9a11f05a74
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-11-07 15:19:28 -0600

    Update paths in generate_test_cert.sh

--- tests/scripts/generate_test_cert.sh
+++ tests/scripts/generate_test_cert.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-openssl req -x509 -nodes -days 3650 \
-    -newkey rsa:2048 -keyout server-key.pem \
-    -out server-cert.pem -config server.cnf
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 openssl req -x509 -nodes -days 3650 \
-    -newkey rsa:2048 -keyout client-key.pem \
-    -out client-cert.pem -config client.cnf
+    -newkey rsa:2048 -keyout "$DIR/../config_files/server-key.pem" \
+    -out "$DIR/../config_files/server-cert.pem" -config "$DIR/../config_files/server.cnf"
+
+openssl req -x509 -nodes -days 3650 \
+    -newkey rsa:2048 -keyout "$DIR/../config_files/client-key.pem" \
+    -out "$DIR/../config_files/client-cert.pem" -config "$DIR/../config_files/client.cnf"
