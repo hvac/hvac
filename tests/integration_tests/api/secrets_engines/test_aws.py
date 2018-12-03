88e2e0fddb02a3a372168c0f76d3437b2541fd51
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-12-03 13:18:09 -0600

    First pass on aws secrets engine tests

--- tests/integration_tests/api/secrets_engines/test_aws.py
+++ tests/integration_tests/api/secrets_engines/test_aws.py
@@ -1,75 +1,285 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+from threading import Thread
 from unittest import TestCase
 
-from parameterized import parameterized
+from parameterized import parameterized, param
 
-from hvac.api.secrets_engines.aws import DEFAULT_MOUNT_POINT
-from hvac.tests import utils
+from tests import utils
+from tests.utils.hvac_integration_test_case import HvacIntegrationTestCase
+from tests.utils.mock_aws_request_handler import MockAWSRequestHandler
+
+try:
+    # Python 2.7
+    from http.server import HTTPServer
+except ImportError:
+    # Python 3.x
+    from BaseHTTPServer import HTTPServer
 
 
-class TestAws(utils.HvacIntegrationTestCase, TestCase):
+class TestAws(HvacIntegrationTestCase, TestCase):
+    TEST_MOUNT_POINT = 'aws-test'
+    TEST_ROLE_NAME = 'hvac-test-role'
 
     @classmethod
     def setUpClass(cls):
         super(TestAws, cls).setUpClass()
+        # Configure mock server.
+        cls.mock_server_port = utils.get_free_port()
+        cls.mock_server = HTTPServer(('localhost', cls.mock_server_port), MockAWSRequestHandler)
+
+        # Start running mock server in a separate thread.
+        # Daemon threads automatically shut down when the main process exits.
+        cls.mock_server_thread = Thread(target=cls.mock_server.serve_forever)
+        cls.mock_server_thread.setDaemon(True)
+        cls.mock_server_thread.start()
 
     def setUp(self):
         super(TestAws, self).setUp()
+        if '%s/' % self.TEST_MOUNT_POINT not in self.client.list_auth_backends():
+            self.client.sys.enable_secrets_engine(
+                backend_type='aws',
+                path=self.TEST_MOUNT_POINT,
+            )
 
     def tearDown(self):
+        self.client.sys.disable_secrets_engine(
+            path=self.TEST_MOUNT_POINT,
+        )
         super(TestAws, self).tearDown()
 
-    
     @parameterized.expand([
-        ('some_test',),
+        param(
+            'success',
+        ),
     ])
-    def test_configure_root_iam_credentials(self, test_label):
-        raise NotImplementedError
-    
+    def test_configure_root_iam_credentials(self, label, credentials='', raises=None, exception_message=''):
+        access_key = 'butts'
+        secret_key = 'secret-butts'
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.secrets.aws.configure_root_iam_credentials(
+                    access_key=access_key,
+                    secret_key=secret_key,
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception),
+            )
+        else:
+            configure_response = self.client.secrets.aws.configure_root_iam_credentials(
+                access_key=access_key,
+                secret_key=secret_key,
+                iam_endpoint='localhost',
+                sts_endpoint='localhost',
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('configure_response: %s' % configure_response)
+            self.assertEqual(
+                first=configure_response.status_code,
+                second=204,
+            )
+
     @parameterized.expand([
-        ('some_test',),
+        param(
+            'success',
+        ),
     ])
-    def test_rotate_root_iam_credentials(self, test_label):
-        raise NotImplementedError
-    
+    def test_configure_lease(self, label, lease='60s', lease_max='120s', raises=None, exception_message=''):
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.secrets.aws.configure_lease(
+                    lease=lease,
+                    lease_max=lease_max,
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception),
+            )
+        else:
+            configure_response = self.client.secrets.aws.configure_lease(
+                lease=lease,
+                lease_max=lease_max,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('configure_response: %s' % configure_response)
+            self.assertEqual(
+                first=configure_response.status_code,
+                second=204,
+            )
+
     @parameterized.expand([
-        ('some_test',),
+        param(
+            'success',
+        ),
     ])
-    def test_configure_lease(self, test_label):
-        raise NotImplementedError
-    
+    def test_read_lease(self, label, lease='60s', lease_max='120s', configure_first=True, raises=None, exception_message=''):
+        if configure_first:
+            configure_response = self.client.secrets.aws.configure_lease(
+                lease=lease,
+                lease_max=lease_max,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('configure_response: %s' % configure_response)
+
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.secrets.aws.read_lease_config(
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception),
+            )
+        else:
+            read_response = self.client.secrets.aws.read_lease_config(
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('read_response: %s' % read_response)
+            self.assertEqual(
+                first=int(lease_max.replace('s', '')),
+                second=self.convert_python_ttl_value_to_expected_vault_response(
+                    ttl_value=read_response['data']['lease_max'],
+                ),
+            )
+
     @parameterized.expand([
-        ('some_test',),
+        param(
+            'success',
+        ),
     ])
-    def test_read_lease(self, test_label):
-        raise NotImplementedError
-    
+    def test_create_or_update_role(self, label, credential_type='iam_user', policy_document=None, default_sts_ttl=None,
+                                   max_sts_ttl=None, role_arns=None, policy_arns=None, raises=None,
+                                   exception_message=''):
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.secrets.aws.create_or_update_role(
+                    name=self.TEST_ROLE_NAME,
+                    credential_type=credential_type,
+                    policy_document=policy_document,
+                    default_sts_ttl=default_sts_ttl,
+                    max_sts_ttl=max_sts_ttl,
+                    role_arns=role_arns,
+                    policy_arns=policy_arns,
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception),
+            )
+        else:
+            role_response = self.client.secrets.aws.create_or_update_role(
+                name=self.TEST_ROLE_NAME,
+                credential_type=credential_type,
+                policy_document=policy_document,
+                default_sts_ttl=default_sts_ttl,
+                max_sts_ttl=max_sts_ttl,
+                role_arns=role_arns,
+                policy_arns=policy_arns,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('role_response: %s' % role_response)
+            self.assertEqual(
+                first=role_response.status_code,
+                second=204,
+            )
+
     @parameterized.expand([
-        ('some_test',),
+        param(
+            'success',
+        ),
     ])
-    def test_create_or_update_role(self, test_label):
-        raise NotImplementedError
-    
+    def test_read_role(self, label, configure_first=True, raises=None, exception_message=''):
+        if configure_first:
+            self.client.secrets.aws.create_or_update_role(
+                name=self.TEST_ROLE_NAME,
+                credential_type='iam_user',
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.secrets.aws.read_role(
+                    name=self.TEST_ROLE_NAME,
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception),
+            )
+        else:
+            read_role_response = self.client.secrets.aws.read_role(
+                name=self.TEST_ROLE_NAME,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('read_role_response: %s' % read_role_response)
+            self.assertEqual(
+                first=read_role_response['data']['credential_types'],
+                second=['iam_user'],
+            )
+
     @parameterized.expand([
-        ('some_test',),
+        param(
+            'success',
+        ),
     ])
-    def test_read_role(self, test_label):
-        raise NotImplementedError
-    
+    def test_list_roles(self, label, configure_first=True, raises=None, exception_message=''):
+        if configure_first:
+            self.client.secrets.aws.create_or_update_role(
+                name=self.TEST_ROLE_NAME,
+                credential_type='iam_user',
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.secrets.aws.list_roles(
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception),
+            )
+        else:
+            list_roles_response = self.client.secrets.aws.list_roles(
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('list_roles_response: %s' % list_roles_response)
+            self.assertEqual(
+                first=list_roles_response['data']['keys'],
+                second=[self.TEST_ROLE_NAME],
+            )
+
     @parameterized.expand([
-        ('some_test',),
+        param(
+            'success',
+        ),
     ])
-    def test_list_roles(self, test_label):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    def test_delete_role(self, test_label):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    def test_generate_credentials(self, test_label):
-        raise NotImplementedError
-    
+    def test_delete_role(self, label, configure_first=True, raises=None, exception_message=''):
+        if configure_first:
+            self.client.secrets.aws.create_or_update_role(
+                name=self.TEST_ROLE_NAME,
+                credential_type='iam_user',
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.secrets.aws.delete_role(
+                    name=self.TEST_ROLE_NAME,
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception),
+            )
+        else:
+            delete_role_response = self.client.secrets.aws.delete_role(
+                name=self.TEST_ROLE_NAME,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('delete_role_response: %s' % delete_role_response)
+            self.assertEqual(
+                first=delete_role_response.status_code,
+                second=204,
+            )
--- tests/unit_tests/api/secrets_engines/test_aws.py
+++ tests/unit_tests/api/secrets_engines/test_aws.py
@@ -1,86 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
 from unittest import TestCase
 
 import requests_mock
-from parameterized import parameterized
+from parameterized import parameterized, param
 
 from hvac.adapters import Request
 from hvac.api.secrets_engines import Aws
 from hvac.api.secrets_engines.aws import DEFAULT_MOUNT_POINT
+from hvac.exceptions import ParamValidationError
 
 
 class TestAws(TestCase):
+    @parameterized.expand([
+        param(
+            'success',
+        ),
+    ])
+    def test_rotate_root_iam_credentials(self, test_label, mount_point=DEFAULT_MOUNT_POINT):
+        expected_status_code = 200
+        mock_response = {
+          "data": {
+            "access_key": "AKIA..."
+          }
+        }
+        aws = Aws(adapter=Request())
+        mock_url = 'http://localhost:8200/v1/{mount_point}/config/rotate-root'.format(
+            mount_point=mount_point,
+        )
+        logging.debug('Mocking URL: %s' % mock_url)
+        with requests_mock.mock() as requests_mocker:
+            requests_mocker.register_uri(
+                method='POST',
+                url=mock_url,
+                status_code=expected_status_code,
+                json=mock_response,
+            )
+            rotate_root_response = aws.rotate_root_iam_credentials(
+                mount_point=mount_point,
+            )
+        logging.debug('rotate_root_response: %s' % rotate_root_response)
+        self.assertEqual(
+            first=mock_response,
+            second=rotate_root_response,
+        )
 
-    @classmethod
-    def setUpClass(cls):
-        super(TestAws, cls).setUpClass()
+    @parameterized.expand([
+        param(
+            'success',
+        ),
+        param(
+            'invalid endpoint',
+            endpoint='cats',
+            raises=ParamValidationError,
+            exception_msg='cats'
+        ),
+    ])
+    def test_generate_credentials(self, test_label, role_name='hvac-test-role', mount_point=DEFAULT_MOUNT_POINT, endpoint='creds', raises=None, exception_msg=''):
+        expected_status_code = 200
+        mock_response = {
+          "data": {
+            "access_key": "AKIA...",
+            "secret_key": "xlCs...",
+            "security_token": None
+          }
+        }
+        mock_url = 'http://localhost:8200/v1/{mount_point}/creds/{role_name}'.format(
+            mount_point=mount_point,
+            role_name=role_name,
+        )
+        logging.debug('Mocking URL: %s' % mock_url)
+        aws = Aws(adapter=Request())
+        with requests_mock.mock() as requests_mocker:
+            requests_mocker.register_uri(
+                method='GET',
+                url=mock_url,
+                status_code=expected_status_code,
+                json=mock_response,
+            )
 
-    def setUp(self):
-        super(TestAws, self).setUp()
-
-    def tearDown(self):
-        super(TestAws, self).tearDown()
-
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    @requests_mock.Mocker()
-    def test_configure_root_iam_credentials(self, test_label, requests_mocker):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    @requests_mock.Mocker()
-    def test_rotate_root_iam_credentials(self, test_label, requests_mocker):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    @requests_mock.Mocker()
-    def test_configure_lease(self, test_label, requests_mocker):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    @requests_mock.Mocker()
-    def test_read_lease(self, test_label, requests_mocker):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    @requests_mock.Mocker()
-    def test_create_or_update_role(self, test_label, requests_mocker):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    @requests_mock.Mocker()
-    def test_read_role(self, test_label, requests_mocker):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    @requests_mock.Mocker()
-    def test_list_roles(self, test_label, requests_mocker):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    @requests_mock.Mocker()
-    def test_delete_role(self, test_label, requests_mocker):
-        raise NotImplementedError
-    
-    @parameterized.expand([
-        ('some_test',),
-    ])
-    @requests_mock.Mocker()
-    def test_generate_credentials(self, test_label, requests_mocker):
-        raise NotImplementedError
-    
+            if raises:
+                with self.assertRaises(raises) as cm:
+                    aws.generate_credentials(
+                        name=role_name,
+                        endpoint=endpoint,
+                        mount_point=mount_point,
+                    )
+                self.assertIn(
+                    member=exception_msg,
+                    container=str(cm.exception),
+                )
+            else:
+                gen_creds_response = aws.generate_credentials(
+                    name=role_name,
+                    endpoint=endpoint,
+                    mount_point=mount_point,
+                )
+                logging.debug('gen_creds_response: %s' % gen_creds_response)
+                self.assertEqual(
+                    first=mock_response,
+                    second=gen_creds_response,
+                )
