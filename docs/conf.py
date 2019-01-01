b643ff44691da67da35cdab6646c0a8fdbedb0a4
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2019-01-01 17:16:55 -0600

    Bump version: 0.7.1 → 0.7.2

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@ copyright = u'2018-2019, Ian Unruh, Jeffrey Hogan'
 author = u'Ian Unruh, Jeffrey Hogan'
 
 # The short X.Y version
-version = '0.7.1'
+version = '0.7.2'
 # The full version, including alpha/beta/rc tags
-release = '0.7.1'
+release = '0.7.2'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def load_long_description():
 
 setup(
     name='hvac',
-    version='0.7.1',
+    version='0.7.2',
     description='HashiCorp Vault API client',
     long_description=load_long_description(),
     long_description_content_type="text/markdown",
