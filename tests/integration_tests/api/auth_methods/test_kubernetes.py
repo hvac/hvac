3fca767aaec1eb6b71491a3aa07c0301ecc2cd79
Jakub Spórna <jakub.sporna@ac-project.net>
2019-03-26 04:41:11 +0100

    add tests for Kubernetes auth method

--- tests/integration_tests/api/auth_methods/test_kubernetes.py
+++ tests/integration_tests/api/auth_methods/test_kubernetes.py
@@ -2,73 +2,296 @@ import logging
 from unittest import TestCase
 from unittest import skipIf
 
-import requests_mock
-from parameterized import parameterized
+from parameterized import parameterized, param
 
-from hvac.adapters import Request
-from hvac.api.auth_methods import Kubernetes
+from hvac import exceptions
 from tests import utils
+from tests.utils.hvac_integration_test_case import HvacIntegrationTestCase
 
 
 @skipIf(utils.vault_version_lt('0.8.3'), "Kubernetes auth method not available before Vault version 0.8.3")
-class TestKubernetes(TestCase):
+class TestKubernetes(HvacIntegrationTestCase, TestCase):
     TEST_MOUNT_POINT = 'kubernetes-test'
 
-    @parameterized.expand([
-        ('success', dict(), None,),
-    ])
-    @requests_mock.Mocker()
-    def test_login(self, label, test_params, raises, requests_mocker):
-        role_name = 'hvac'
-        test_policies = [
-            "default",
-            "dev",
-            "prod",
-        ]
-        expected_status_code = 200
-        mock_url = 'http://localhost:8200/v1/auth/{mount_point}/login'.format(
+    def setUp(self):
+        super(TestKubernetes, self).setUp()
+        if '%s/' % self.TEST_MOUNT_POINT not in self.client.list_auth_backends():
+            self.client.enable_auth_backend(
+                backend_type='kubernetes',
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+
+    def tearDown(self):
+        super(TestKubernetes, self).tearDown()
+        self.client.disable_auth_backend(
             mount_point=self.TEST_MOUNT_POINT,
         )
-        mock_response = {
-            "auth": {
-                "client_token": "38fe9691-e623-7238-f618-c94d4e7bc674",
-                "accessor": "78e87a38-84ed-2692-538f-ca8b9f400ab3",
-                "policies": test_policies,
-                "metadata": {
-                    "role": role_name,
-                    "service_account_name": "vault-auth",
-                    "service_account_namespace": "default",
-                    "service_account_secret_name": "vault-auth-token-pd21c",
-                    "service_account_uid": "aa9aa8ff-98d0-11e7-9bb7-0800276d99bf"
-                },
-                "lease_duration": 2764800,
-                "renewable": True,
-            },
-        }
-        requests_mocker.register_uri(
-            method='POST',
-            url=mock_url,
-            status_code=expected_status_code,
-            json=mock_response,
+
+    @parameterized.expand([
+        param(
+            'success',
+
+        ),
+        param(
+            'set invalid kubernetes_ca_cert',
+            kubernetes_ca_cert='ca_cert',
+            raises=exceptions.ParamValidationError,
+            exception_message='required type: PEM'
+        ),
+        param(
+            'set invalid pem_key',
+            kubernetes_ca_cert='-----BEGIN CERTIFICATE-----\\n.....\\n-----END CERTIFICATE-----',
+            pem_keys=['pem_key'],
+            raises=exceptions.ParamValidationError,
+            exception_message='required type: PEM'
+        ),
+        param(
+            'set invalid token_reviewer_jwt',
+            kubernetes_ca_cert='-----BEGIN CERTIFICATE-----\\n.....\\n-----END CERTIFICATE-----',
+            token_reviewer_jwt='reviewer_jwt',
+            raises=exceptions.InternalServerError,
+            exception_message='* not a compact JWS'
         )
-        kubernetes = Kubernetes(adapter=Request())
+    ])
+    def test_configure(self, label, kubernetes_ca_cert=None, token_reviewer_jwt=None, pem_keys=None,
+                       raises=None, exception_message=''):
+        kubernetes_host = 'https://192.168.99.100:8443'
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.auth.kubernetes.configure(
+                    kubernetes_host=kubernetes_host,
+                    kubernetes_ca_cert=kubernetes_ca_cert,
+                    token_reviewer_jwt=token_reviewer_jwt,
+                    pem_keys=pem_keys,
+                    mount_point=self.TEST_MOUNT_POINT
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception)
+            )
+        else:
+            configure_response = self.client.auth.kubernetes.configure(
+                kubernetes_host=kubernetes_host,
+                kubernetes_ca_cert='-----BEGIN CERTIFICATE-----\\n.....\\n-----END CERTIFICATE-----',
+                mount_point=self.TEST_MOUNT_POINT
+            )
+            logging.debug('configure_response: %s' % configure_response)
+            self.assertEqual(
+                first=configure_response.status_code,
+                second=204,
+            )
+
+    @parameterized.expand([
+        param(
+            'success',
+        ),
+        param(
+            'no config written yet',
+            write_config_first=False,
+            raises=exceptions.InvalidPath
+        )
+    ])
+    def test_read_config(self, label, write_config_first=True, raises=None):
+        expected_config = {
+            'kubernetes_host': 'https://192.168.99.100:8443',
+            'kubernetes_ca_cert': '-----BEGIN CERTIFICATE-----\\n.....\\n-----END CERTIFICATE-----',
+        }
+        if write_config_first:
+            self.client.auth.kubernetes.configure(
+                mount_point=self.TEST_MOUNT_POINT,
+                **expected_config
+            )
         if raises is not None:
             with self.assertRaises(raises):
-                kubernetes.login(
-                    role=role_name,
-                    jwt='my-jwt',
+                self.client.auth.kubernetes.read_config(
                     mount_point=self.TEST_MOUNT_POINT,
-                    **test_params
                 )
         else:
-            login_response = kubernetes.login(
-                role=role_name,
-                jwt='my-jwt',
+            read_config_response = self.client.auth.kubernetes.read_config(
                 mount_point=self.TEST_MOUNT_POINT,
-                **test_params
             )
-            logging.debug('login_response: %s' % login_response)
+            logging.debug('read_config_response: %s' % read_config_response)
+            for k, v in expected_config.items():
+                self.assertEqual(
+                    first=v,
+                    second=read_config_response[k],
+                )
+
+    @parameterized.expand([
+        param(
+            'success',
+            bound_service_account_names=['vault-auth'],
+            bound_service_account_namespaces=['default'],
+        ),
+        param(
+            'both bounds wildcard',
+            bound_service_account_names=['*'],
+            bound_service_account_namespaces=['*'],
+            raises=exceptions.ParamValidationError,
+        ),
+    ])
+    def test_create_role(self, label, bound_service_account_names=None, bound_service_account_namespaces=None,
+                         raises=None, exception_message=''):
+        role_name = 'test-role'
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.auth.kubernetes.create_role(
+                    name=role_name,
+                    bound_service_account_names=bound_service_account_names,
+                    bound_service_account_namespaces=bound_service_account_namespaces,
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception),
+            )
+        else:
+            create_role_response = self.client.auth.kubernetes.create_role(
+                name=role_name,
+                bound_service_account_names=bound_service_account_names,
+                bound_service_account_namespaces=bound_service_account_namespaces,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('create_role_response: %s' % create_role_response)
             self.assertEqual(
-                first=login_response['auth']['policies'],
-                second=test_policies,
+                first=create_role_response.status_code,
+                second=204,
+            )
+
+    @parameterized.expand([
+        param(
+            'success',
+        ),
+        param(
+            'nonexistent role name',
+            create_role_first=False,
+            raises=exceptions.InvalidPath,
+        ),
+    ])
+    def test_read_role(self, label, create_role_first=True, raises=None, exception_message=''):
+        role_name = 'test-role'
+        expected_role_config = {
+            'name': role_name,
+            'bound_service_account_names': ['vault-auth'],
+            'bound_service_account_namespaces': ['default'],
+        }
+        role_name = 'test-role'
+
+        if create_role_first:
+            self.client.auth.kubernetes.create_role(
+                mount_point=self.TEST_MOUNT_POINT,
+                **expected_role_config
+            )
+        if raises:
+            with self.assertRaises(raises) as cm:
+                self.client.auth.kubernetes.read_role(
+                    name=role_name,
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+            self.assertIn(
+                member=exception_message,
+                container=str(cm.exception),
+            )
+        else:
+            read_role_response = self.client.auth.kubernetes.read_role(
+                name=role_name,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('read_role_response: %s' % read_role_response)
+            self.assertEqual(
+                first=read_role_response['bound_service_account_names'],
+                second=expected_role_config['bound_service_account_names']
+            )
+            self.assertEqual(
+                first=read_role_response['bound_service_account_namespaces'],
+                second=expected_role_config['bound_service_account_namespaces']
+            )
+
+    @parameterized.expand([
+        # param(
+        #     'success',  # TODO: figure out why this is returning a "InvalidPath" exception ("unsupported path")
+        # ),
+        param(
+            'no roles',
+            num_roles_to_create=0,
+            raises=exceptions.InvalidPath,
+        ),
+        param(
+            'no config',
+            write_config_first=False,
+            raises=exceptions.InvalidPath,
+        ),
+    ])
+    def test_list_roles(self, label, num_roles_to_create=1, write_config_first=True, raises=None):
+        if write_config_first:
+            self.client.auth.kubernetes.configure(
+                kubernetes_host='https://192.168.99.100:8443',
+                kubernetes_ca_cert='-----BEGIN CERTIFICATE-----\n.....\n-----END CERTIFICATE-----',
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+        roles_to_create = ['hvac{}'.format(str(n)) for n in range(0, num_roles_to_create)]
+        bound_service_account_names = ['vault-auth']
+        bound_service_account_namespaces = ['default']
+        logging.debug('roles_to_create: %s' % roles_to_create)
+        for role_to_create in roles_to_create:
+            create_role_response = self.client.auth.kubernetes.create_role(
+                name=role_to_create,
+                bound_service_account_names=bound_service_account_names,
+                bound_service_account_namespaces=bound_service_account_namespaces,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('create_role_response: %s' % create_role_response)
+
+        if raises:
+            with self.assertRaises(raises):
+                self.client.auth.kubernetes.list_roles(
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+        else:
+            list_roles_response = self.client.auth.kubernetes.list_roles(
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('list_roles_response: %s' % list_roles_response)
+            self.assertEqual(
+                first=list_roles_response['keys'],
+                second=roles_to_create,
+            )
+
+    @parameterized.expand([
+        param(
+            'success',
+        ),
+        param(
+            'nonexistent role name',
+            configure_role_first=False,
+        ),
+    ])
+    def test_delete_role(self, label, configure_role_first=True, raises=None):
+        role_name = 'test-role'
+        bound_service_account_names = ['vault-auth']
+        bound_service_account_namespaces = ['default']
+        if configure_role_first:
+            create_role_response = self.client.auth.kubernetes.create_role(
+                name=role_name,
+                bound_service_account_names=bound_service_account_names,
+                bound_service_account_namespaces=bound_service_account_namespaces,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('create_role_response: %s' % create_role_response)
+
+        if raises is not None:
+            with self.assertRaises(raises):
+                self.client.auth.kubernetes.delete_role(
+                    name=role_name,
+                    mount_point=self.TEST_MOUNT_POINT,
+                )
+        else:
+            delete_role_response = self.client.auth.kubernetes.delete_role(
+                name=role_name,
+                mount_point=self.TEST_MOUNT_POINT,
+            )
+            logging.debug('delete_role_response: %s' % delete_role_response)
+            self.assertEqual(
+                first=delete_role_response.status_code,
+                second=204,
             )
