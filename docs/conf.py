1a4a003fe5252da9c6ff965749a16d9ddc3eef61
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2019-05-20 12:55:06 -0500

    Rename forked doctest ext to reduce confusion

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ release = '0.8.2'
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-    'docs.ext.doctest',
+    'docs.ext.hvac_doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
