256f983132339f1699286543c1f224202728f54d
Drew Mullen <drew@nebulaworks.com>
2019-05-29 15:35:31 -0400

    include db engine as implemented class

--- hvac/api/secrets_engines/__init__.py
+++ hvac/api/secrets_engines/__init__.py
@@ -8,6 +8,7 @@ from hvac.api.secrets_engines.pki import Pki
 from hvac.api.secrets_engines.kv_v1 import KvV1
 from hvac.api.secrets_engines.kv_v2 import KvV2
 from hvac.api.secrets_engines.transit import Transit
+from hvac.api.secrets_engines.database import Database
 from hvac.api.vault_api_category import VaultApiCategory
 
 __all__ = (
@@ -21,6 +22,7 @@ __all__ = (
     'Pki',
     'Transit',
     'SecretsEngines',
+    'Database'
 )
 
 
@@ -35,13 +37,13 @@ class SecretsEngines(VaultApiCategory):
         Kv,
         Pki,
         Transit,
+        Database,
     ]
     unimplemented_classes = [
         'Ad',
         'AliCloud',
         'Azure',
         'Consul',
-        'Database',
         'GcpKms',
         'Nomad',
         'RabbitMq',
