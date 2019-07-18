70ceed8c50eecaa690ede192abe5111ca6a1307d
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2019-07-18 10:02:46 -0500

    Bump version: 0.9.3 → 0.9.4

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@ copyright = u'2018-2019, Ian Unruh, Jeffrey Hogan'
 author = u'Ian Unruh, Jeffrey Hogan'
 
 # The short X.Y version
-version = '0.9.3'
+version = '0.9.4'
 # The full version, including alpha/beta/rc tags
-release = '0.9.3'
+release = '0.9.4'
 
 
 # -- General configuration ---------------------------------------------------
