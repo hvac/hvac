c7453a543e3b2ddaea93eb79af2f8b0f3e94ce73
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2019-07-18 14:57:37 -0500

    Organize imports

--- hvac/api/secrets_engines/__init__.py
+++ hvac/api/secrets_engines/__init__.py
@@ -1,16 +1,16 @@
 """Vault secrets engines endpoints"""
+from hvac.api.secrets_engines.active_directory import ActiveDirectory
 from hvac.api.secrets_engines.aws import Aws
 from hvac.api.secrets_engines.azure import Azure
+from hvac.api.secrets_engines.consul import Consul
+from hvac.api.secrets_engines.database import Database
 from hvac.api.secrets_engines.gcp import Gcp
-from hvac.api.secrets_engines.active_directory import ActiveDirectory
 from hvac.api.secrets_engines.identity import Identity
 from hvac.api.secrets_engines.kv import Kv
-from hvac.api.secrets_engines.pki import Pki
 from hvac.api.secrets_engines.kv_v1 import KvV1
 from hvac.api.secrets_engines.kv_v2 import KvV2
+from hvac.api.secrets_engines.pki import Pki
 from hvac.api.secrets_engines.transit import Transit
-from hvac.api.secrets_engines.database import Database
-from hvac.api.secrets_engines.consul import Consul
 from hvac.api.vault_api_category import VaultApiCategory
 
 __all__ = (
