b89bfe5409fba0f529d58718283fa99810d5ced4
llamasoft <llamasoft@rm-rf.email>
2019-10-15 16:58:36 -0700

    Overhaul Vault/Consul install scripts

--- tests/scripts/install-consul.sh
+++ tests/scripts/install-consul.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -eux
 
-DEFAULT_CONSUL_VERSION=1.4.0
-HVAC_CONSUL_VERSION=${1:-$DEFAULT_CONSUL_VERSION}
+DEFAULT_CONSUL_VERSION="1.4.0"
+DEFAULT_CONSUL_DIRECTORY="${HOME}/bin"
+HVAC_CONSUL_VERSION="${1:-$DEFAULT_CONSUL_VERSION}"
+HVAC_CONSUL_DIRECTORY="${2:-$DEFAULT_CONSUL_DIRECTORY}"
 
 function install_consul_release() {
-    mkdir -p $HOME/bin
+    cd "/tmp"
 
-    cd /tmp
+    download_url="https://releases.hashicorp.com/consul/${HVAC_CONSUL_VERSION}/consul_${HVAC_CONSUL_VERSION}_linux_amd64.zip"
+    download_file="consul_${HVAC_CONSUL_VERSION}.zip"
+    curl -sL "${download_url}" -o "${download_file}"
+    unzip "${download_file}"
 
-    curl -sOL https://releases.hashicorp.com/consul/${HVAC_CONSUL_VERSION}/consul_${HVAC_CONSUL_VERSION}_linux_amd64.zip
-    unzip consul_${HVAC_CONSUL_VERSION}_linux_amd64.zip
-    mv consul $HOME/bin
+    mkdir -p "${HVAC_CONSUL_DIRECTORY}"
+    mv "consul" "${HVAC_CONSUL_DIRECTORY}"
 }
 
 install_consul_release
