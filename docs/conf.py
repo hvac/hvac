2db1280ccf2269df07633373a01f6f90dae40855
Jeffrey Hogan <jhogan@hashicorp.com>
2020-07-26 12:44:36 -0500

    Bump version: 0.10.4 → 0.10.5

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,9 @@ copyright = u'2018-2020, Ian Unruh, Jeffrey Hogan'
 author = u'Ian Unruh, Jeffrey Hogan'
 
 # The short X.Y version
-version = '0.10.4'
+version = '0.10.5'
 # The full version, including alpha/beta/rc tags
-release = '0.10.4'
+release = '0.10.5'
 
 
 # -- General configuration ---------------------------------------------------
