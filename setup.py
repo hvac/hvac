9bd4d00f8745e2f3f1571eb7888e497134f0a849
Jeffrey Hogan <jhogan@hashicorp.com>
2021-07-10 18:01:23 -0500

    `python_requires=">=3.6"`

--- setup.py
+++ setup.py
@@ -21,14 +21,12 @@ setup(
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     packages=find_packages(exclude=["docs*", "tests*"]),
-    python_requires=">=2.7",
+    python_requires=">=3.6",
     install_requires=[
         "requests>=2.21.0",
         "six>=1.5.0",
