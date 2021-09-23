cdbd0ef176b6c7ff725a91d5a2e4ef8cddf881bd
Jeffrey Hogan <jhogan@hashicorp.com>
2021-09-23 12:33:00 -0500

    Revert "Drop six from requirements"
    This reverts commit c9ff353f9bea91476edf70da243c4df98d2b3612.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     python_requires=">=2.7",
     install_requires=[
         "requests>=2.21.0",
+        "six>=1.5.0",
     ],
     include_package_data=True,
     package_data={"hvac": ["version"]},
