b1cfd22f94ae21bd3e2d5f1c694269b88a262d73
Kevin Hollywood <khollywood@datto.com>
2022-05-06 17:38:58 -0400

    Bump version: 0.11.2 → 0.11.3

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@ copyright = u"2018-2020, Ian Unruh, Jeffrey Hogan"
 author = u"Ian Unruh, Jeffrey Hogan"
 
 # The short X.Y version
-version = "0.11.2"
+version = "0.11.3"
 # The full version, including alpha/beta/rc tags
-release = "0.11.2"
+release = "0.11.3"
 
 
 # -- General configuration ---------------------------------------------------
