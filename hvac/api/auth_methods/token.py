715f6b77a45dba2e29fc5b0cf828cacfba03b86b
Marti Raudsepp <marti@juffo.org>
2022-07-28 18:39:43 +0300

    Update to new parameter names per Vault API doc https://www.vaultproject.io/api-docs/auth/token#create-update-token-role

--- hvac/api/auth_methods/token.py
+++ hvac/api/auth_methods/token.py
@@ -555,8 +555,8 @@ class Token(VaultApiBase):
         renewable=True,
         path_suffix=None,
         allowed_entity_aliases=None,
-        period=None,
-        explicit_max_ttl=None,
+        token_period=None,
+        token_explicit_max_ttl=None,
         mount_point=DEFAULT_MOUNT_POINT,
     ):
         """Create (or replace) the named role.
@@ -588,10 +588,10 @@ class Token(VaultApiBase):
         :type path_suffix: str
         :param allowed_entity_aliases: not case sensitive.
         :type allowed_entity_aliases: str
-        :param period: the token will have no maximum TTL, every renewal will use the given period.
-        :type period: str
-        :param explicit_max_ttl: the token cannot be renewed past this TTL value.
-        :type explicit_max_ttl: str
+        :param token_period: the token will have no maximum TTL, every renewal will use the given period.
+        :type token_period: str
+        :param token_explicit_max_ttl: the token cannot be renewed past this TTL value.
+        :type token_explicit_max_ttl: str
         :param mount_point: The "path" the method/backend was mounted on.
         :type mount_point: str
         :return: The response of the create_or_update_role request.
@@ -605,8 +605,8 @@ class Token(VaultApiBase):
                 "renewable": renewable,
                 "path_suffix": path_suffix,
                 "allowed_entity_aliases": allowed_entity_aliases,
-                "period": period,
-                "explicit_max_ttl": explicit_max_ttl,
+                "token_period": token_period,
+                "token_explicit_max_ttl": token_explicit_max_ttl,
             }
         )
         api_path = "/v1/auth/{mount_point}/roles/{role_name}".format(
