63708c1aad7daab600e4543c947cd045ac9b0aff
Brian Scholer <1260690+briantist@users.noreply.github.com>
2022-09-25 16:51:35 -0400

    add tests for fallback methods

--- tests/unit_tests/adapters/responses/test_hvac_adapter_response.py
+++ tests/unit_tests/adapters/responses/test_hvac_adapter_response.py
@@ -14,6 +14,24 @@ def mock_response():
     return mock.Mock(spec=Response)
 
 
+@pytest.fixture
+def filled_response(mock_response):
+    data = {
+        "data1": "value1",
+        "data2": "value2",
+    }
+    mock_response.status_code = 200
+    mock_response.json.return_value = data
+
+    return HvacAdapterResponse(mock_response)
+
+
+@pytest.fixture
+def mock_warn():
+    with mock.patch('warnings.warn') as w:
+        yield w
+
+
 class TestHvacAdapterResponse:
     def test_is_adapter_response(self):
         assert issubclass(HvacAdapterResponse, RequestsAdapterResponse)
@@ -76,3 +94,45 @@ class TestHvacAdapterResponse:
         assert rar._value is None
 
         mock_response.json.assert_called_once()
+
+    # test deprecated fallback stuff
+
+    def test_general_attr_access(self, mock_response, filled_response, mock_warn):
+        v = filled_response.get("data1")
+
+        mock_warn.assert_called_once()
+        assert v == mock_response.json()["data1"]
+        assert v == filled_response.value["data1"]
+
+    def test_getitem(self, mock_response, filled_response, mock_warn):
+        v = filled_response["data1"]
+
+        mock_warn.assert_called_once()
+        assert v == mock_response.json()["data1"]
+        assert v == filled_response.value["data1"]
+
+    def test_setitem(self, mock_response, filled_response, mock_warn):
+        v = "a1"
+        filled_response["new1"] = v
+
+        mock_warn.assert_called_once()
+        assert len(filled_response.value) == 3
+        assert v == filled_response.value["new1"]
+        assert v == mock_response.json()["new1"]
+
+    def test_delitem(self, mock_response, filled_response, mock_warn):
+        del filled_response["data2"]
+
+        mock_warn.assert_called_once()
+        assert len(filled_response.value) == 1
+        assert "data2" not in filled_response.value
+        assert "value1" == filled_response.value["data1"]
+        assert "value1" == mock_response.json()["data1"]
+
+    def test_len(self, filled_response, mock_warn):
+        assert len(filled_response) == len(filled_response.value)
+        mock_warn.assert_called_once()
+
+    def test_in(self, filled_response, mock_warn):
+        assert "data2" in filled_response
+        mock_warn.assert_called_once()
