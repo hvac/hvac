27dad5b0b8b436d85ea68ecb57d294692dc34591
Brian Scholer <1260690+briantist@users.noreply.github.com>
2023-12-30 16:01:15 -0500

    fix readthedocs (RTD) public docsite, modernize some of the docs build/testing (#1119)
    * empty
    
    * empty
    
    * empty
    
    * constrain jinja2 for docs builds
    
    * try newer m2r3 range
    
    * new mock
    
    * add pyhcl docs req
    
    * undo pyhcl req
    
    * remove pyhcl req
    
    * move docs requirements to poetry group
    
    * use the new docs group
    
    * fix mock import
    
    * use same poetry
    
    * remove deprecated call
    
    * fix reqs
    
    * fix docs lang
    
    * replace find_executable with which
    
    * remove unused file
    
    * update contributing docs

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ source_suffix = [".rst", ".md"]
 # The master toctree document.
 master_doc = "index"
 
-language = None
+language = "en"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 pygments_style = "sphinx"
 
@@ -87,7 +87,7 @@ doctest_global_setup = """
 import os
 from pprint import pprint, pformat
 
-import mock
+import unittest.mock
 
 import hvac
 from tests import utils as test_utils
