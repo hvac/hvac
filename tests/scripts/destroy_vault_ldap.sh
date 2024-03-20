deb7d5b277e1c8d3e7ccf1027fc466b07ea28349
Jordan Stopford <jordan.stopford@cristie.com>
2024-03-20 11:01:09 +0000

    Reverting the changes prior to implementing unit tests

--- tests/scripts/destroy_vault_ldap.sh
+++
@@ -1,3 +0,0 @@
-terraform -chdir="`dirname $0`"/../config_files/vault-ldap init || :
-terraform -chdir="`dirname $0`"/../config_files/vault-ldap destroy -auto-approve || :
-docker compose -f "`dirname $0`"/../config_files/vault-ldap/docker-compose.yml down vault openldap
--- tests/scripts/init_vault_ldap.sh
+++
@@ -1,3 +0,0 @@
-docker compose -f "`dirname $0`"/../config_files/vault-ldap/docker-compose.yml up -d vault openldap
-terraform -chdir="`dirname $0`"/../config_files/vault-ldap init
-terraform -chdir="`dirname $0`"/../config_files/vault-ldap apply -auto-approve
